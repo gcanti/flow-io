e14e9af89792c3b223a6ee2ac51ff09c3453cf31
gcanti <giulio.canti@gmail.com>
2016-10-14 13:24:23 +0200

    test literal

---
+++ test/literal.js
@@ -0,0 +1,23 @@
+// @flow
+
+declare var describe: (title: string, f: () => void) => void;
+declare var it: (title: string, f: () => void) => void;
+
+import * as t from '../src/index'
+import { assertValidationFailure, assertValidationSuccess } from './helpers'
+
+describe('literal', () => {
+
+  it('should succeed validating a valid value', () => {
+    const T = t.literal({ value: 'a' })
+    assertValidationSuccess(t.validate('a', T))
+  })
+
+  it('should fail validating an invalid value', () => {
+    const T = t.literal({ value: 'a' })
+    assertValidationFailure(t.validate(1, T), [
+      'Invalid value 1 supplied to : "a"'
+    ])
+  })
+
+})
