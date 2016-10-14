30884847f2dd882efd3b720bcf7936e352da4524
gcanti <giulio.canti@gmail.com>
2016-10-14 11:59:24 +0200

    test $shape

---
+++ test/$shape.js
@@ -0,0 +1,34 @@
+// @flow
+
+declare var describe: (title: string, f: () => void) => void;
+declare var it: (title: string, f: () => void) => void;
+
+import * as t from '../src/index'
+import { assertValidationFailure, assertValidationSuccess } from './helpers'
+
+describe('$shape', () => {
+
+  it('should succeed validating a valid value', () => {
+    const T = t.$shape(t.object({ a: t.string }))
+    assertValidationSuccess(t.validate({}, T))
+    assertValidationSuccess(t.validate({ a: 's' }, T))
+  })
+
+  it('should fail validating an invalid value', () => {
+    const T = t.$shape(t.object({ a: t.string }))
+    assertValidationFailure(t.validate(1, T), [
+      'Invalid value 1 supplied to : $Shape<{ a: string }>'
+    ])
+    assertValidationFailure(t.validate({ a: 1 }, T), [
+      'Invalid value 1 supplied to : $Shape<{ a: string }>/a: string'
+    ])
+  })
+
+  it('should check for additional props', () => {
+    const T = t.$shape(t.object({ a: t.string }))
+    assertValidationFailure(t.validate({ a: 's', additional: 2 }, T), [
+      'Invalid value 2 supplied to : $Shape<{ a: string }>/additional: nil'
+    ])
+  })
+
+})
