7f7fe41ce57a22c8b57358454f907605ea21d28f
Giulio Canti <giulio.canti@gmail.com>
2016-10-24 16:04:17 +0200

    add support for $ExactType and $ShapeType to $keys (#12)

--- test/$keys.js
+++ test/$keys.js
@@ -8,16 +8,29 @@ import { assertValidationFailure, assertValidationSuccess } from './helpers'
 
 describe('$keys', () => {
 
-  it('should succeed validating a valid value', () => {
+  it('should succeed validating a valid value (object type)', () => {
     const T = t.$keys(t.object({ a: t.string, b: t.number }))
     assertValidationSuccess(t.validate('a', T))
     assertValidationSuccess(t.validate('b', T))
   })
 
-  it('should fail validating an invalid value', () => {
+  it('should succeed validating a valid value (exact object type)', () => {
+    const T = t.$keys(t.$exact({ a: t.string, b: t.number }))
+    assertValidationSuccess(t.validate('a', T))
+    assertValidationSuccess(t.validate('b', T))
+  })
+
+  it('should fail validating an invalid value (object type)', () => {
     const T = t.$keys(t.object({ a: t.string, b: t.number }))
     assertValidationFailure(t.validate('c', T), [
       'Invalid value "c" supplied to : $Keys<{ a: string, b: number }>'
+    ])
+  })
+
+  it('should fail validating an invalid value (exact object type)', () => {
+    const T = t.$keys(t.$exact({ a: t.string, b: t.number }))
+    assertValidationFailure(t.validate('c', T), [
+      'Invalid value "c" supplied to : $Keys<$Exact<{ a: string, b: number }>>'
     ])
   })
 
